02c8380839e304e649a589a0c198bb17085988a4
okellomano <onyandookello@gmail.com>
2024-03-19 14:34:49 +0300

    adding objects

--- summing_numbers.py
+++ summing_numbers.py
@@ -18,6 +18,10 @@ def my_average(numbers):
 
 
 def list_of_words(words):
+    '''Takes a list of words and returns a tuple conatining the length of the shortest word, longest word, and
+        the average length of words. 
+     
+     '''
     if not words:
         return (0, 0, 0)
     
@@ -40,8 +44,23 @@ def list_of_words(words):
     
     return (shortest_length, longest_length, average_length)
 
+def sum_objects(elements, start=0):
+    '''Takes a list of objects and sums objects that either are integers or can be converted to integers, while ignoring the rest. '''
+    total = start
+    
+    for element in elements:
+        try:
+            num = int(element)
+            total += num
+        except (ValueError, TypeError):
+            pass
+
+    return total
+
 
 if __name__ == '__main__':
     print(my_sum([1, 2, 3, 6], 8))
     print(f"For words: {list_of_words(['to', 'take', 'somebody', 'love', 'you', 'trees'])}")
     # print(f'The average is: {my_average(1, 2, 3, 6, 7)}')
+    
+    print(sum_objects([4, 6, '8', 'string']))
