6d8286fbdc587d49c527bce41a6aa1caf57011a8
okellomano <onyandookello@gmail.com>
2024-05-03 14:15:16 +0300

    even odds position sums

--- list_tuples/first_last.py
+++ list_tuples/first_last.py
@@ -14,5 +14,23 @@ def firstlast(sequence):
     return sequence[:1] + sequence[-1:]
 
 
+'''
+Write a function that takes a list or tuple of numbers. 
+Return a two-element list,containing (respectively) the sum of the even-indexed numbers and the sum ofthe odd-indexed numbers. 
+So calling the function as even_odd_sums([10,20,30,40,50,60]), you’ll get back [90,120].
+
+'''
+
+def even_odd_sums(numbers):
+    if len(numbers) == 1:
+        return numbers
+    
+    if type(numbers) == list:
+        return [sum(numbers[0::2]), sum(numbers[1::2])]
+    elif type(numbers) == tuple:
+        return sum(numbers[0::2]), sum(numbers[1::2])
+
+
 if __name__ == '__main__':
-    print(firstlast([1]))
+    print(firstlast([1, 2, 3, 4]))
+    print(f'\nEven Odds Sum: {even_odd_sums([10,20,30,40,50,60])}')
