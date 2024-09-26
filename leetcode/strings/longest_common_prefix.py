51803793baa2afd9abf4e6e8dadaeee8ca4b1864
okellomano <onyandookello@gmail.com>
2024-09-26 21:00:59 +0300

    longest substring

--- leetcode/strings/longest_common_prefix.py
+++ leetcode/strings/longest_common_prefix.py
@@ -21,3 +21,21 @@ Constraints:
 0 <= strs[i].length <= 200
 strs[i] consists of only lowercase English letters'''
 
+
+class Solution:
+    def longest_common_prefix(self, strs: List[str]) -> str:
+        shortest_string = min(strs, key=len)
+        length = len(shortest_string)
+        
+        longest_substring = ""
+        
+        for i in range(length):
+            for j in range(i + 1, length + 1):
+                substring = shortest_string[i:j]
+                
+                if all(substring in s for s in strs):
+                    if len(substring) > len(longest_substring):
+                        longest_substring = substring
+        return longest_substring
+    
+
